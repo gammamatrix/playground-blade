e0b5aeefc2ead58e2c0f11500f86d556a68ea1a3
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2024-03-05 00:10:30 -0800

    GH-11

---
+++ src/Assets/Comment.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Playground
+ */
+namespace Playground\Blade\Assets;
+
+use Playground\Blade\Assets\Contracts\Comment as CommentContract;
+
+/**
+ * \Playground\Blade\Assets\Style
+ */
+class Comment extends Asset implements CommentContract
+{
+    protected bool $always = false;
+
+    protected string $comment = '';
+
+    public function setOptions(array $options = []): self
+    {
+        $this->always = ! empty($options['always']);
+
+        if (! empty($options['comment'])
+            && is_string($options['comment'])
+        ) {
+            $this->comment = $options['comment'];
+        }
+
+        return $this;
+    }
+
+    public function always(): bool
+    {
+        return $this->always;
+    }
+
+    public function comment(): string
+    {
+        return $this->comment;
+    }
+
+    public function __toString(): string
+    {
+        return sprintf('<!-- %1$s -->', $this->comment());
+        // return sprintf('<!-- %1$s -->', e($this->commentq()));
+    }
+}
