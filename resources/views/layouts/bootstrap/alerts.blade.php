efdefb0a2c86bdb0f23e697b2831ee5498b9fcef
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2025-11-22 15:44:56 -0800

    v74.0.0

--- resources/views/layouts/bootstrap/alerts.blade.php
+++ resources/views/layouts/bootstrap/alerts.blade.php
@@ -1,12 +1,18 @@
-<div class="container alerts-container">
+<div class="container-fluid alerts-container">
     <div class="row justify-content-center" id="alerts">
-        @foreach (['danger', 'warning', 'success', 'info', 'primary', 'secondary', 'tertiary', 'quaternary', 'light', 'dark'] as $key)
-            @if (Session::has($key))
-                <div class="alert alert-{{ $key }} col mt-3 alert-block alert-dismissible fade show">
-                    <strong>{{ Session::get($key) }}</strong>
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            @endif
-        @endforeach
+        <div class="col-md-12">
+            @foreach (['command', 'danger', 'warning', 'success', 'info', 'primary', 'secondary', 'tertiary', 'quaternary', 'light', 'dark'] as $key)
+                @if (Session::has($key))
+                    <div class="alert alert-{{ $key }} col mt-3 mb-3 alert-block alert-dismissible fade show">
+                        @if ($key === 'command')
+                            <pre class="user-select-all">{!!  Session::get($key) !!}</pre>
+                        @else
+                            <strong>{!!  Session::get($key) !!}</strong>
+                        @endif
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
+            @endforeach
+        </div>
     </div>
 </div>
